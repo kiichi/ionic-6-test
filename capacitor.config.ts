5ec003519cb33cc4ab48705146bcddc39503e8c4
Kiichi Takeuchi <kiichi.takeuchi@iprocedures.com>
2022-04-21 15:25:01 -0400

    ionic 6 test

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-6-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
